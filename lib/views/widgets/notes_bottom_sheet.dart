6bce5d09b88d8f5fe4bcaba8a2ded645c8a91577
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-17 15:09:57 +0200

    18. Separate Note's Input into NoteForm Widget to Reuse it

--- lib/views/widgets/notes_bottom_sheet.dart
+++ lib/views/widgets/notes_bottom_sheet.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'main_elevated_button.dart';
-import 'main_text_form_field.dart';
+import 'note_form.dart';
 
 class NotesBottomSheet extends StatelessWidget {
   const NotesBottomSheet({super.key});
@@ -13,16 +13,7 @@ class NotesBottomSheet extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            MainTextFormField(
-              label: 'Title',
-              onSaved: (value) {},
-            ),
-            const SizedBox(height: 16),
-            MainTextFormField(
-              label: 'Content',
-              maxLines: 5,
-              onSaved: (value) {},
-            ),
+            const NoteForm(),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 32),
               child: MainElevatedButton(
