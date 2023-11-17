d233fb0e4cb26ffc1c57e7ffab2d3b131cf23547
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-17 15:06:46 +0200

    17. Reuse AppBarButton & Global AppBarTheme in EditNoteView

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/app_bar_button.dart';
+
 class EditNoteView extends StatelessWidget {
   const EditNoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Note'),
+        actions: [
+          AppBarButton(
+            icon: const Icon(Icons.check_rounded),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
