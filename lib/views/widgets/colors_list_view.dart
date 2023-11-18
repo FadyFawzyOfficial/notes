57e8c882373b79e827d820170b8aa40c4cfef8d0
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-18 22:11:22 +0200

    2. Split ColorItem Into Separate Widget & Convert ColorsListView to Stateful

--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  var selectedColorIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +17,42 @@ class ColorsListView extends StatelessWidget {
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
         itemCount: 10,
-        itemBuilder: (context, index) => const CircleAvatar(
-          radius: 24,
-          backgroundColor: Colors.amber,
+        itemBuilder: (context, index) => InkWell(
+          borderRadius: const BorderRadius.all(Radius.circular(24)),
+          onTap: () => setState(() => selectedColorIndex = index),
+          child: ColorItem(
+            color: Colors.teal,
+            isSelected: selectedColorIndex == index,
+          ),
         ),
-        separatorBuilder: (context, index) => const SizedBox(width: 4),
+        separatorBuilder: (context, index) => const SizedBox(width: 8),
       ),
     );
   }
+}
+
+class ColorItem extends StatelessWidget {
+  final Color color;
+  final bool isSelected;
+
+  const ColorItem({
+    super.key,
+    required this.color,
+    required this.isSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) => isSelected
+      ? CircleAvatar(
+          radius: 24,
+          backgroundColor: Colors.white,
+          child: CircleAvatar(
+            radius: 22,
+            backgroundColor: color,
+          ),
+        )
+      : CircleAvatar(
+          radius: 24,
+          backgroundColor: color,
+        );
 }
