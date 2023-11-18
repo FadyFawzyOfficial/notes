946be93655a710630f9aebd6b01654a4f6b795f7
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-18 08:14:49 +0200

    13. Build Simple Bloc Observer to Watch & Track Cubits Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'constants/strings.dart';
 import 'models/note.dart';
+import 'simple_bloc_observer.dart';
 import 'views/notes_view.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteAdapter());
