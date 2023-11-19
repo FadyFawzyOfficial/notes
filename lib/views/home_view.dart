0e88330cbaceb5ee5daf1b6e7459383ef2428406
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-19 16:50:59 +0200

    2. Rename NotesFab to HomeFab & Remove Search IconButton

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/app_bar_button.dart';
-import 'widgets/notes_fab.dart';
+import 'widgets/home_fab.dart';
 import 'widgets/notes_list_view.dart';
 
 class HomeView extends StatelessWidget {
@@ -10,17 +9,9 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes'),
-        actions: [
-          AppBarButton(
-            icon: const Icon(Icons.search_rounded),
-            onPressed: () {},
-          )
-        ],
-      ),
+      appBar: AppBar(title: const Text('Notes')),
       body: const NotesListView(),
-      floatingActionButton: const NotesFab(),
+      floatingActionButton: const HomeFab(),
     );
   }
 }
